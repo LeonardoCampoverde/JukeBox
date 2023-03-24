f2f91fb3c16f00c68c3a5fd0b3dc556e9c691c7c
elmiao <leonardocampoverdeprof@gmail.com>
2023-03-24 19:08:28 +0100

    risolto il bug del precedente commit

--- app/src/main/java/com/example/jokeboxleonardocampoverde/numGenerato.java
+++ app/src/main/java/com/example/jokeboxleonardocampoverde/numGenerato.java
@@ -1,74 +1,48 @@
-package com.example.jokeboxleonardocampoverde;
+﻿package com.example.jokeboxleonardocampoverde;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 public class numGenerato extends AppCompatActivity {
 
-    //lista delle canzoni
-    final String[] canzoni= new String[]{
-            "Brividi",
-            "As it was",
-            "Giovani Wannabe",
-            "Cuff it",
-            "Shakerando",
-            "Importante",
-            "The Loneliest",
-            "Non lo sai",
-            "Le pietre non volano",
-            "Bam bam",
-            "Bellissima",
-            "Bloody Mary",
-            "Buonanotte",
-            "Despechà",
-            "Titi me preguntò",
-            "BZRP Music Sessions #52",
-            "Bagno a mezzanotte",
-            "Running up that hill",
-            "Cold Heart",
-            "La dolce vita",
-    };
 
 
-    //lista dei link alle canzoni
-    final String[] link= {
-            "https://www.youtube.com/watch?v=MA_5P3u0apQ",
-            "https://www.youtube.com/watch?v=H5v3kku4y6Q",
-            "https://www.youtube.com/watch?v=4GXDFtuG9Xo",
-            "https://www.youtube.com/watch?v=yrtWLyp5gLI",
-            "https://www.youtube.com/watch?v=U6VMjHJ18a0",
-            "https://www.youtube.com/watch?v=tTaEfEyEGrQ",
-            "https://www.youtube.com/watch?v=odWKEfp2QMY",
-            "https://www.youtube.com/watch?v=JQgHPTTXr0I",
-            "https://www.youtube.com/watch?v=kEn6YsuH0KE",
-            "https://www.youtube.com/watch?v=-8VfKZCOo_I",
-            "https://www.youtube.com/watch?v=qz88Dx-_lA4",
-            "https://www.youtube.com/watch?v=MsXdUtlDVhk",
-            "https://www.youtube.com/watch?v=osuKmCiGy1w",
-            "https://www.youtube.com/watch?v=5g2hT4GmAGU",
-            "https://www.youtube.com/watch?v=Cr8K88UcO0s",
-            "https://www.youtube.com/watch?v=A_g3lMcWVy0",
-            "https://www.youtube.com/watch?v=XSHcZ4rcBDk",
-            "https://www.youtube.com/watch?v=wp43OdtAAkM",
-            "https://www.youtube.com/watch?v=qod03PVTLqk",
-            "https://www.youtube.com/watch?v=TX_csdgqhxA",
-    };
+
+
+
 
     //dichiaro l'oggetto Button che mi servirà per aggiungere il listener
     private Button ascolta;
+
+
+    private MediaPlayer mediaPlayer;
+    private boolean riproduzione = false;
+    private ImageButton player;
+
+    Canzone [] c = Canzone.init();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.num_generato);
+
+
+        player=findViewById(R.id.player);
+
 
 
 
@@ -86,11 +60,12 @@ public class numGenerato extends AppCompatActivity {
         }
 
         //ottengo il valore NUMERICO (int) del numero
-        int intNum=extras.getInt("intNum");
+
+        int intNum=getIntent().getIntExtra("intNum",0);
 
         //trovo tramite id la textview che in cui ci sarà il nome della canzone
         TextView canzone = (TextView)findViewById(R.id.nome_canzone);
-        canzone.setText(canzoni[intNum]);//imposto il nome della canzone
+        canzone.setText(c[intNum].nome);//imposto il nome della canzone
         //trovo tramite id il bottone in cui assocerò il link da aprire della rispettiva canzone
         ascolta = findViewById(R.id.ascolta);
         //imposto la funzione da chiamare quando il bottone è premuto
@@ -100,6 +75,8 @@ public class numGenerato extends AppCompatActivity {
                 ascoltaMusica();//chiamo la funzione
             }
         });
+        mediaPlayer = MediaPlayer.create(this, c[intNum].path);
+        player.setImageResource(R.drawable.play);
 
 
 
@@ -109,14 +86,39 @@ public class numGenerato extends AppCompatActivity {
 
 
     public void ascoltaMusica(){
+        int intNum=getIntent().getIntExtra("intNum",0);
         //creo una nuova activity e apro il link della canzone
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(link[getIntent().getIntExtra("intNum",0)]));
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(c[intNum].link));
         //avvio l'activity
         startActivity(intent);
     }
 
+    public void riproduci(View view){
+        Log.d("NUMGENERATO","FUNZIONE CHIAMATA: riproduci");
+        if(riproduzione){
+            mediaPlayer.pause();
+            riproduzione=false;
+            player.setImageResource(R.drawable.play);
+            Log.d("NUMGENERATO",String.valueOf(player.getId()));
+        }
+        else{
+            mediaPlayer.start();
+            riproduzione=true;
+            player.setImageResource(R.drawable.pause);
+
+        }
+    }
 
 
+
+
+
+    @Override
+    protected void onDestroy(){
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        super.onDestroy();
+    }
 
     //crea la freccetta in alto a sinistra per andare indietro
 
